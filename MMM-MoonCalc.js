04ea56dc58ec4d05303bb839fec5902968ba9569
Mykle <25211804+mykle1@users.noreply.github.com>
2018-02-20 11:54:31 -0500

    Add files via upload

--- MMM-MoonCalc.js
+++ MMM-MoonCalc.js
@@ -33,6 +33,7 @@ Module.register("MMM-MoonCalc", {
 
         //  Set locale.
         this.MoonCalc = {};
+		this.Lune = {};
         this.scheduleUpdate();
     },
 	
@@ -57,6 +58,7 @@ Module.register("MMM-MoonCalc", {
         }
 
         var MoonCalc = this.MoonCalc;
+		var Lune = this.Lune;
 		
 
         var top = document.createElement("div");
@@ -113,6 +115,66 @@ Module.register("MMM-MoonCalc", {
 		
 		
 		
+		// spacer
+        var spacer = document.createElement("div");
+        spacer.classList.add("small", "bright", "spacer");
+        spacer.innerHTML = " ~ ~ ~ ~ ~ ";
+        wrapper.appendChild(spacer);
+		
+		
+		
+		// Lune Phase
+        var phase = document.createElement("div");
+        phase.classList.add("small", "bright", "lunePhase");
+        phase.innerHTML = "Lune Phase " + Number(Math.round(Lune.phase+'e2')+'e-2') + " %";
+        wrapper.appendChild(phase);
+		
+		
+		// Lune illuminated
+        var illuminated = document.createElement("div");
+        illuminated.classList.add("small", "bright", "illuminated");
+        illuminated.innerHTML = "The moon is " + Number(Math.round(Lune.illuminated+'e2')+'e-2') + "% illuminated";
+        wrapper.appendChild(illuminated);
+		
+		
+		// luneAge
+        var luneAge = document.createElement("div");
+        luneAge.classList.add("small", "bright", "luneAge");
+        luneAge.innerHTML = "Lunar cycle is " + Number(Math.round(Lune.age+'e2')+'e-2') + " days old";
+        wrapper.appendChild(luneAge);
+		
+		
+		// luneDistance
+        var luneDistance = document.createElement("div");
+        luneDistance.classList.add("small", "bright", "luneDistance");
+        luneDistance.innerHTML = "Distance from Earth is " + Number(Math.round(Lune.distance+'e2')+'e-2') + " km";
+        wrapper.appendChild(luneDistance);
+		
+		
+		// angular_diameter
+        var angular_diameter = document.createElement("div");
+        angular_diameter.classList.add("small", "bright", "angular_diameter");
+        angular_diameter.innerHTML = "Angular diameter of moon is " + Number(Math.round(Lune.angular_diameter+'e2')+'e-2') + " &deg";
+        wrapper.appendChild(angular_diameter);
+		
+		
+		// sun_distance
+        var sun_distance = document.createElement("div");
+        sun_distance.classList.add("small", "bright", "sun_distance");
+        sun_distance.innerHTML = "Distance to the sun is " + Number(Math.round(Lune.sun_distance+'e2')+'e-2') + " km";
+        wrapper.appendChild(sun_distance);
+		
+		
+		// sun_angular_diameter
+        var sun_angular_diameter = document.createElement("div");
+        sun_angular_diameter.classList.add("small", "bright", "sun_angular_diameter");
+        sun_angular_diameter.innerHTML = "Angular diameter of sun is " + Number(Math.round(Lune.sun_angular_diameter+'e2')+'e-2') + " &deg";
+        wrapper.appendChild(sun_angular_diameter);
+		
+		
+		
+		
+		
         return wrapper;
 		
     }, // closes getDom
@@ -135,6 +197,11 @@ Module.register("MMM-MoonCalc", {
 	//	console.log(this.MoonCalc); // for checking
         this.loaded = true;
     },
+	
+	processLune: function(data) {
+        this.Lune = data; 
+	//	console.log(this.Lune); // for checking in dev console
+    },
 
     scheduleUpdate: function() {
         setInterval(() => {
@@ -150,8 +217,11 @@ Module.register("MMM-MoonCalc", {
     socketNotificationReceived: function(notification, payload) {
         if (notification === "MOONCALC_RESULT") {
             this.processMoonCalc(payload);
-
             this.updateDom(this.config.animationSpeed);
+        }
+		if (notification === "LUNE_RESULT") {
+            this.processLune(payload);
+            this.updateDom(this.config.fadeSpeed);
         }
         this.updateDom(this.config.initialLoadDelay);
     },
--- node_helper.js
+++ node_helper.js
@@ -6,6 +6,7 @@
  */
 const NodeHelper = require('node_helper');
 var MoonCalc = require('mooncalc');
+var lune = require('lune');
 
 
 module.exports = NodeHelper.create({
@@ -21,11 +22,26 @@ module.exports = NodeHelper.create({
 		var moonDatas = MoonCalc.datasForDay(date);
 	//	console.log(moonDatas);
     	this.sendSocketNotification("MOONCALC_RESULT", moonDatas);
+		
+		this.getLune();
     },
+	
+	
+	getLune: function(url) {
+    //    var self= this;
+		var lune = require('lune');
+		var current_phase = lune.phase();
+		console.log(current_phase);
+    	this.sendSocketNotification("LUNE_RESULT", current_phase);
+    },
+	
 
     socketNotificationReceived: function(notification, payload) {
         if (notification === 'GET_MOONCALC') {
             this.getMoonCalc(payload);
+        }
+		if (notification === 'GET_LUNE') {
+            this.getLune(payload);
         }
     }
 });
